eb7d227a2ca84f771232328788ddc835f420cadc
Hans Joergen Grimstad <hansj66@gmail.com>
2015-05-26 19:37:01 +0200

    Preparing for linear rails
    Also updated configuration with number of steps for Z .

--- Marlin1.0.1/Configuration.h
+++ Marlin1.0.1/Configuration.h
@@ -1,4 +1,4 @@
-#ifndef CONFIGURATION_H
+﻿#ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
 #include "boards.h"
@@ -474,7 +474,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,2570,475}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,2560,475}  // default steps per unit for Ultimaker
 //#define DEFAULT_MAX_FEEDRATE          {120, 120, 2, 120}    // (mm/sec)
 #define DEFAULT_MAX_FEEDRATE          {150, 150, 2, 150}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,50,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
